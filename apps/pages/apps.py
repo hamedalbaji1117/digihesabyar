084ba2b08e57014967f5281f3974ae82c8831c47
Hamed Albaji <h1117h@gmail.com>
2025-11-21 02:36:53 +0330

    Reset repository to start new project

--- apps/pages/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class PagesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.pages"
-
-    def ready(self):
-        import apps.pages.signals
